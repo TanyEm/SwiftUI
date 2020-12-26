7f0989bd5320a9ab4630f263e596bdbfc71b1daa
TanyEm <tanyem@protonmail.com>
2020-12-26 19:51:07 +0200

    Initial Commit

---
+++ Wolt/WoltApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoltApp.swift
+//  Wolt
+//
+//  Created by Tatiana Podlesnykh on 26.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct WoltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
